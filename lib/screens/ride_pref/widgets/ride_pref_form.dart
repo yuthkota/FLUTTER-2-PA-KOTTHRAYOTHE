ab6bc1a8f8917f296b0fed84772a7d2edeb67bc0
yuthkota <147136175+yuthkota@users.noreply.github.com>
2025-02-27 10:42:52 +0700

    BLA003 - Implement the ride pref form

--- lib/screens/ride_pref/widgets/ride_pref_form.dart
+++ lib/screens/ride_pref/widgets/ride_pref_form.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../../../model/ride/locations.dart';
 import '../../../model/ride_pref/ride_pref.dart';
+import '../../../theme/theme.dart';
 import '../../../utils/animations_util.dart';
 import '../../location_picker_screen.dart';
 
@@ -93,71 +94,102 @@ class _RidePrefFormState extends State<RidePrefForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        ListTile(
-          leading: const Icon(Icons.location_on),
-          title: Text(departure?.name ?? 'Leaving from'),
-          onTap: () {
-            Navigator.of(context).push(
-              AnimationUtils.createBottomToTopRoute(
-                LocationPickerScreen(
-                  onLocationSelected: _selectDeparture,
+    return Container(
+      padding: const EdgeInsets.all(BlaSpacings.m),
+      decoration: BoxDecoration(
+        color: BlaColors.white,
+        borderRadius: BorderRadius.circular(BlaSpacings.radius),
+        boxShadow: [
+          BoxShadow(color: Colors.black12, blurRadius: 5, spreadRadius: 1),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          // Departure & Arrival with Switch Button on the Right
+          Row(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    ListTile(
+                      leading:
+                          Icon(Icons.location_on, color: BlaColors.iconNormal),
+                      title: Text(departure?.name ?? 'Leaving from',
+                          style: BlaTextStyles.body
+                              .copyWith(color: BlaColors.textNormal)),
+                      onTap: () {
+                        Navigator.of(context).push(
+                          AnimationUtils.createBottomToTopRoute(
+                            LocationPickerScreen(
+                              onLocationSelected: _selectDeparture,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    Divider(color: BlaColors.disabled),
+                    ListTile(
+                      leading:
+                          Icon(Icons.location_on, color: BlaColors.iconNormal),
+                      title: Text(arrival?.name ?? 'Going to',
+                          style: BlaTextStyles.body
+                              .copyWith(color: BlaColors.textNormal)),
+                      onTap: () {
+                        Navigator.of(context).push(
+                          AnimationUtils.createBottomToTopRoute(
+                            LocationPickerScreen(
+                              onLocationSelected: _selectArrival,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
                 ),
               ),
-            );
-          },
-        ),
-        const Divider(),
-        ListTile(
-          leading: const Icon(Icons.location_on),
-          title: Text(arrival?.name ?? 'Going to'),
-          onTap: () {
-            Navigator.of(context).push(
-              AnimationUtils.createBottomToTopRoute(
-                LocationPickerScreen(
-                  onLocationSelected: _selectArrival,
-                ),
+              IconButton(
+                icon: Icon(Icons.swap_vert, color: BlaColors.primary),
+                onPressed: _switchLocations,
+                tooltip: 'Switch locations',
               ),
-            );
-          },
-        ),
-        const Divider(),
-        IconButton(
-          icon: const Icon(Icons.swap_horiz),
-          onPressed: _switchLocations,
-          tooltip: 'Switch locations',
-        ),
-        const Divider(),
-        ListTile(
-          leading: const Icon(Icons.calendar_today),
-          title: Text(
-            '${departureDate.day}/${departureDate.month}/${departureDate.year}',
+            ],
           ),
-          onTap: () => _selectDate(context),
-        ),
-        const Divider(),
-        ListTile(
-          leading: const Icon(Icons.people),
-          title: const Text('Seats'),
-          trailing: DropdownButton<int>(
-            value: requestedSeats,
-            onChanged: _updateSeats,
-            items: List.generate(4, (index) => index + 1)
-                .map<DropdownMenuItem<int>>((int value) {
-              return DropdownMenuItem<int>(
-                value: value,
-                child: Text(value.toString()),
-              );
-            }).toList(),
+          Divider(color: BlaColors.disabled),
+          // Date Picker
+          ListTile(
+            leading: Icon(Icons.calendar_today, color: BlaColors.iconNormal),
+            title: Text(
+              '${departureDate.day}/${departureDate.month}/${departureDate.year}',
+              style: BlaTextStyles.body.copyWith(color: BlaColors.textNormal),
+            ),
+            onTap: () => _selectDate(context),
           ),
-        ),
-        const Divider(),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: ElevatedButton(
+          Divider(color: BlaColors.disabled),
+          // Seats Selector
+          ListTile(
+            leading: Icon(Icons.person, color: BlaColors.iconNormal),
+            title: Text('$requestedSeats',
+                style:
+                    BlaTextStyles.body.copyWith(color: BlaColors.textNormal)),
+            trailing: DropdownButton<int>(
+              value: requestedSeats,
+              onChanged: _updateSeats,
+              dropdownColor: BlaColors.white,
+              items: List.generate(4, (index) => index + 1)
+                  .map<DropdownMenuItem<int>>((int value) {
+                return DropdownMenuItem<int>(
+                  value: value,
+                  child: Text(value.toString(),
+                      style: BlaTextStyles.body
+                          .copyWith(color: BlaColors.textNormal)),
+                );
+              }).toList(),
+            ),
+          ),
+          const SizedBox(height: BlaSpacings.m),
+          // Search Button
+          ElevatedButton(
             onPressed: () {
               if (departure == null || arrival == null) {
                 ScaffoldMessenger.of(context).showSnackBar(
@@ -176,10 +208,20 @@ class _RidePrefFormState extends State<RidePrefForm> {
               );
               print(ridePref);
             },
-            child: const Text('Search'),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: BlaColors.primary,
+              padding: const EdgeInsets.symmetric(vertical: 14),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(BlaSpacings.radius),
+              ),
+            ),
+            child: Text(
+              'Search',
+              style: BlaTextStyles.button.copyWith(color: BlaColors.white),
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
